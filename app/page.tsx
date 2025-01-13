a959c512fc449d80d905d6635ce26bb76ddd087b
Hilary29 <keubouhilary@gmail.com>
2025-01-13 17:38:02 +0100

    fix text

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,19 @@ export default function Home() {
     <main className="min-h-screen bg-gradient-to-br from-white-50 to-accent-100 bg-cover bg-center w-full">
       <HeaderHome />
       <section 
-        className="flex flex-col items-center w-full px-4 sm:px-6 md:px-8 lg:px-28 pt-16 sm:pt-20 md:pt-24 pb-6"
+        className="flex flex-col items-center w-full px-4 sm:px-6 md:px-8 lg:px-28 pt-28  lg:pt-24 pb-6"
         aria-label="Héros"
       >
-        <div className="flex flex-col lg:flex-row w-full justify-between gap-8 sm:gap-12 md:gap-14 lg:gap-8">
+        <div className="flex flex-col lg:flex-row w-full   justify-between gap-8 sm:gap-12 md:gap-14 lg:gap-8">
           {/* Contenu texte */}
-          <div className="flex flex-col gap-6 sm:gap-8 md:gap-10 lg:my-auto order-2 lg:order-1">
+          <div className="flex flex-col gap-6 sm:gap-8 text-center lg:text-start md:gap-10 lg:my-auto order-2 lg:order-1">
             <h1 
               className="text-2xl sm:text-3xl lg:text-5xl font-semibold font-satoshi text-heading-desktop-h2 text-black-100"
               aria-level={1}
             >
               Génerez facilement des cas cliniques.
             </h1>
-            <p className="font-regular font-inter text-base sm:text-paragraph-lg text-black-400 max-w-none sm:max-w-lg md:max-w-xl">
+            <p className="font-regular font-inter text-base mx-auto lg:mx-0 sm:text-paragraph-lg text-black-400 max-w-none sm:max-w-lg md:max-w-xl">
               Accedez a des cas de patients reels en toute confidentialite. Sélectionnez un intervalle de dates, et obtenez des résultats adaptés à votre contexte.
             </p>
             <div className="flex justify-center lg:justify-start">
